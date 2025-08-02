f2561d8ba6b9180db213f6811b90a83894a48e5f
proctem <evar.umeozor@gmail.com>
2025-08-02 17:49:38 +0100

    Update originalmodela.py

--- originalmodela.py
+++ originalmodela.py
@@ -909,7 +909,7 @@ def MacroEconomic_Model(multiplier, data, location, plant_mode, fund_mode, opex_
 
 ############################################################# ANALYTICS MODEL BEGINS ############################################################
 
-def Analytics_Model2(multiplier, project_data, location, product, plant_mode, fund_mode, opex_mode, carbon_value, plant_size, plant_effy):
+def Analytics_Model2(multiplier, project_data, location, product, plant_mode, fund_mode, opex_mode, carbon_value):
 
   # Filtering data to choose country in which chemical plant is located and the type of product from the plant
   dt = project_data[(project_data['Country'] == location) & (project_data['Main_Prod'] == product) & (project_data['Plant_Size'] == plant_size) & (project_data['Plant_Effy'] == plant_effy)]
